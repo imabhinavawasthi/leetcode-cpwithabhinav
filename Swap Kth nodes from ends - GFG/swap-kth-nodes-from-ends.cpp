49b669d0da8908c582026e99a531385f21e9ce9e
imabhinavawasthi <76579728+imabhinavawasthi@users.noreply.github.com>
2022-04-29 16:49:25 +0530

    Added solution - LeetHub

---
+++ Swap Kth nodes from ends - GFG/swap-kth-nodes-from-ends.cpp
@@ -0,0 +1,120 @@
+// { Driver Code Starts
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+
+Node *swapkthnode(Node* head, int num, int K);
+
+void addressstore(Node **arr, Node* head)
+{
+    Node* temp = head;
+    int i = 0;
+    while(temp){
+        arr[i] = temp;
+        i++;
+        temp = temp->next;
+    }
+}
+
+bool check(Node ** before, Node **after, int num, int K)
+{
+    if(K > num)
+        return 1;
+    return (before[K-1] == after[num - K]) && (before[num-K] == after[K-1]);
+}
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--){
+        int num, K , firstdata;
+        cin>>num>>K;
+        int temp;
+        cin>>firstdata;
+        Node* head = new Node(firstdata);
+        Node* tail = head;
+        for(int i = 0; i<num - 1; i++){
+            cin>>temp;
+            tail->next = new Node(temp);
+            tail = tail->next;
+        }
+        
+        Node *before[num];
+        addressstore(before, head);
+        
+        head = swapkthnode(head, num, K);
+        
+        Node *after[num];
+        addressstore(after, head);
+        
+        if(check(before, after, num, K))
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+/* Linked List Node structure
+   struct Node  {
+     int data;
+     Node *next;
+     Node(int x) {
+        data = x;
+        next = NULL;
+  }
+  }
+*/
+
+//Function to swap Kth node from beginning and end in a linked list.
+Node *swapkthnode(Node* head, int n, int k)
+{
+    // Your Code here
+    if(k>n)return head;
+    Node* x = head;
+    Node* x_prev = NULL;
+    for (int i = 1; i < k; i++) {
+        x_prev = x;
+        x = x->next;
+    }
+    
+    Node* y = head;
+    Node* y_prev = NULL;
+    for (int i = 1; i < n - k + 1; i++) {
+        y_prev = y;
+        y = y->next;
+    }
+    
+    // broken when we change y->next.
+    if (x_prev)
+        x_prev->next = y;
+    // Same thing applies to y_prev
+    if (y_prev)
+        y_prev->next = x;
+        
+    Node* temp = x->next;
+    x->next = y->next;
+    y->next = temp;
+    // Change head pointers when k is 1 or n
+    if (k == 1)
+        head = y;
+    if (k == n)
+        head = x;
+        
+    return head;
+}
