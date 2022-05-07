3b51f9ef4ec14137d37c1299f63cbcaef6fc85b7
imabhinavawasthi <76579728+imabhinavawasthi@users.noreply.github.com>
2022-05-07 12:59:00 +0530

    Added solution - LeetHub

---
+++ 13. Subarray with 0 sum - GFG/13.-subarray-with-0-sum.cpp
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+// A C++ program to find if there is a zero sum
+// subarray
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    //Complete this function
+    //Function to check whether there is a subarray present with 0-sum or not.
+    bool subArrayExists(int arr[], int n)
+    {
+        //Your code here
+        int pre[n+1];
+        pre[0]=0;
+        for(int i=0;i<n;i++){
+            pre[i+1]=pre[i]+arr[i];
+        }
+        map<int,int>mp;
+        for(int i=0;i<=n;i++){
+            int d=pre[i];
+            if(mp[d]>0){
+                return true;
+            }
+            mp[pre[i]]=1;
+        }
+        return false;
+    }
+};
+
+// { Driver Code Starts.
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    Solution obj;
+	    	if (obj.subArrayExists(arr, n))
+		cout << "Yes\n";
+	else
+		cout << "No\n";
+	}
+	return 0;
+}  // } Driver Code Ends
