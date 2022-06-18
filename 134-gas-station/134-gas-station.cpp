8d83c84df51eff71a9c96e0673b29c8c48117838
imabhinavawasthi <76579728+imabhinavawasthi@users.noreply.github.com>
2022-06-18 17:18:25 +0530

    Time: 670 ms (5.14%), Space: 71.2 MB (5.92%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        priority_queue<int>pq;
         int n=gas.size();
+        priority_queue<int>q;
         if(n==1){
             if(gas[0]>=cost[0])return 0;
             else return -1;
         }
         for(int i=0;i<n;i++){
-            if(gas[i]>cost[i])pq.push(i);
+            if(gas[i]>cost[i])q.push(i);
         }
-        while(!pq.empty()){
-            int t=pq.top();
-            pq.pop();
+        while(!q.empty()){
+            int f=q.top();
+            q.pop();
             int c=0;
             int gastank=0;
-            int i=t;
+            int i=f;
             while(true){
-                if(c==n)return t;
+                if(c==n){
+                    return f;
+                }
                 int gaspresent=gas[i];
                 int gasreq=cost[i];
                 gastank+=gaspresent;
                 gastank-=gasreq;
-                if(gastank<0){
-                    break;
-                }
-                c++;
-                // i++;
-                // if(i==n)i=0;
+                //gas present in our tank
+                if(gastank<0)break;
                 i=(i+1)%n;
+                // if(i==n)i=0;
+                c++;
             }
         }
         return -1;
