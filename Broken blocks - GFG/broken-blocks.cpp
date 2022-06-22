d314a701b05425b371312653d8db39fc346b1030
imabhinavawasthi <76579728+imabhinavawasthi@users.noreply.github.com>
2022-06-22 15:00:10 +0530

    Added solution - LeetHub

--- Broken blocks - GFG/broken-blocks.cpp
+++ Broken blocks - GFG/broken-blocks.cpp
@@ -1,44 +1,37 @@
 // { Driver Code Starts
-
 #include<bits/stdc++.h>
 using namespace std;
 
  // } Driver Code Ends
-
 class Solution {
 public:
 int MaxGold(vector<vector<int>>&matrix){
-    // Code here
     int n=matrix.size();
     int m=matrix[0].size();
-    int mx=0;
     for(int i=1;i<n;i++){
         for(int j=0;j<m;j++){
             if(matrix[i][j]==-1)continue;
-            int lastmx=-1;
-                if(j>0&&matrix[i-1][j-1]!=-1){
-                    lastmx=max(lastmx,matrix[i-1][j-1]);
+            else{
+                int mx=-1;
+                if(j>0)mx=max(mx,matrix[i-1][j-1]);
+                if(j<m-1)mx=max(mx,matrix[i-1][j+1]);
+                mx=max(mx,matrix[i-1][j]);
+                if(mx==-1){
+                    matrix[i][j]=-1;
                 }
-                if(matrix[i-1][j]!=-1){
-                    lastmx=max(lastmx,matrix[i-1][j]);
+                else{
+                    matrix[i][j]+=mx;
                 }
-                if(j<m-1&&matrix[i-1][j+1]!=-1){
-                    lastmx=max(lastmx,matrix[i-1][j+1]);
-                }
-                if(lastmx==-1)matrix[i][j]=-1;
-                else
-                matrix[i][j]+=lastmx;
+            }
         }
     }
+    int ans=0;
     for(int i=0;i<n;i++){
-        int rowmx=-1;
         for(int j=0;j<m;j++){
-            rowmx=max(rowmx,matrix[i][j]);
+            ans=max(ans,matrix[i][j]);
         }
-        if(rowmx==-1)return mx;
-        else mx=max(mx,rowmx);
     }
-    return mx;
+    return ans;
 }
 };
 
