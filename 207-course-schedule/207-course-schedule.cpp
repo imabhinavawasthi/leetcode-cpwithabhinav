7e77e7a0fac32d16e50da983dacdc8d4440d4f55
imabhinavawasthi <76579728+imabhinavawasthi@users.noreply.github.com>
2022-06-28 14:22:45 +0530

    Time: 38 ms (36.93%), Space: 13.8 MB (60.17%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,47 +1,38 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) 
-    {
-        vector<vector<int>> adj(numCourses);
-        for(int i = 0; i < prerequisites.size(); i++)
-        {
-            int u = prerequisites[i][0];
-            int v = prerequisites[i][1];
-            adj[u].push_back(v);
-        }
+    bool iscycle(int i,vector<vector<int>>&adj,vector<bool>&stack,vector<bool>&visited){
+        stack[i]=true;
         
-        vector<bool> visited(numCourses, false);
-        vector<bool> stack(numCourses, false);
-        
-        for(int i = 0; i < numCourses; i++)
-        {
-            if(!visited[i] && isCyclic(i, visited, stack, adj))
-                return false;
-        }
-        
-        return true;
-    }
-    
-    bool isCyclic(int src, vector<bool>& visited, vector<bool>& stack, vector<vector<int>>& adj)
-    {
-        stack[src] = true;
-        
-        if(!visited[src])
-        {
-            visited[src] = true;
+        if(!visited[i]){
+            visited[i]=true;
             
-            for(auto it : adj[src])
-            {
-                if(!visited[it] && isCyclic(it, visited, stack, adj))
-                    return true;
-                
-                if(stack[it])
+            for(auto it:adj[i]){
+                if(!visited[it]&&iscycle(it,adj,stack,visited)){
+                return true;//it contains a cycle
+                }
+                if(stack[it]==true)
                     return true;
             }
         }
         
-        stack[src] = false;
-        
+        stack[i]=false;
         return false;
+    }
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        vector<vector<int>>adj(n);
+        for(int i=0;i<pre.size();i++){
+            //x->y [x,y]
+            int x=pre[i][0];
+            int y=pre[i][1];
+            adj[x].push_back(y);
+        }
+        vector<bool>visited(n,false);
+        vector<bool>stack(n,false);
+        for(int i=0;i<n;i++){
+            if(!visited[i]&&iscycle(i,adj,stack,visited)){
+                return false;//it contains a cycle
+            }
+        }
+        return true;//dag
     }
 };
