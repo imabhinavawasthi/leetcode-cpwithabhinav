0cc093e4d8821af10e3a333572168282ec8f96d0
imabhinavawasthi <76579728+imabhinavawasthi@users.noreply.github.com>
2022-07-01 19:41:25 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -8,22 +8,58 @@ class Solution
 {
     public:
     //Function to return max value that can be put in knapsack of capacity W.
-    int knapSack(int W, int wt[], int val[], int n) 
-    { 
-        int dp[W + 1];
-        memset(dp, 0, sizeof(dp));
      
-        for (int i = 1; i < n + 1; i++) {
-            for (int w = W; w >= 0; w--) {
+    // Returns the value of maximum profit
+    int knapSackRec(int W, int wt[],
+                    int val[], int i,
+                    int** dp)
+    {
+        // base condition
+        if (i < 0)
+            return 0;
+        if (dp[i][W] != -1)
+            return dp[i][W];  //reduce our tc
      
-                if (wt[i - 1] <= w)
-                    // finding the maximum value
-                    {int take= (dp[w - wt[i - 1]] + val[i - 1]);
-                    int nottake=dp[w];
-                    dp[w] = max(take,nottake);}
-            }
+        if (wt[i] > W) {
+     
+            // Store the value of function call
+            // stack in table before return
+            dp[i][W] = knapSackRec(W, wt,
+                                   val, i - 1,
+                                   dp);
+            return dp[i][W];
         }
-        return dp[W]; // returning the maximum value of knapsack
+        else {
+            // Store value in a table before return
+            dp[i][W] = max(val[i]
+                          + knapSackRec(W - wt[i],
+                                       wt, val,
+                                       i - 1, dp),
+                           knapSackRec(W, wt, val,
+                                       i - 1, dp));
+     
+            // Return value of table after storing
+            return dp[i][W];
+        }
+    }
+     
+    int knapSack(int W, int wt[], int val[], int n)
+    {
+        // double pointer to declare the
+        // table dynamically
+        int** dp;
+        dp = new int*[n];
+     
+        // loop to create the table dynamically
+        for (int i = 0; i < n; i++)
+            dp[i] = new int[W + 1];
+     
+        // loop to initially filled the
+        // table with -1
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < W + 1; j++)
+                dp[i][j] = -1;
+        return knapSackRec(W, wt, val, n - 1, dp);
     }
 };
 
